d34882267262bdaf3302670c9a44cde22375ac62
EugeniuPeruzzi <jaka1997@live.it>
2023-05-31 17:10:29 +0200

    bonus 2, fixed

---
+++ bonus 2/js/script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let imgs = [
+    './img/wayne-barnett-founder-ceo.jpg',
+    './img/angela-caroll-chief-editor.jpg',
+    './img/walter-gordon-office-manager.jpg',
+    './img/angela-lopez-social-media-manager.jpg',
+    './img/scott-estrada-developer.jpg',
+    './img/barbara-ramos-graphic-designer.jpg'
+];
+
+let objs =[
+    
+    {
+        'name' : 'Wayne Barnett',
+        'role' : 'Founder & CEO',
+        'immage' : imgs[0],
+    },
+
+    {
+        'name' : 'Angela Caroll',
+        'role' : 'Chief Editor',
+        'immage' : imgs[1],
+    },
+
+    {
+        'name' : 'Walter Gordon',
+        'role' : 'Office Manager',
+        'immage' : imgs[2],
+    },
+
+    {
+        'name' : 'Angela Lopez',
+        'role' : 'Social Media Manager',
+        'immage' : imgs[3],
+    },
+
+    {
+        'name' : 'Scott Estrada',
+        'role' : 'Developer',
+        'immage' : imgs[4],
+    },
+
+    {
+        'name' : 'Barbara Ramos',
+        'role' : 'Graphic Designer',
+        'immage' : imgs[5],
+    },
+    
+];
+
+//stampo in c.log le informazionmi degli ogetti
+for (let i = 0; i<objs.length; i++){
+    
+    let objsvar = objs[i]
+    
+    console.log(objsvar);
+
+    let display = document.querySelector('.display');
+    display.innerHTML += 
+    `<div class="name-display m-3 bg-color-white">
+        <div class="img-conainer w-100">
+            <img src="${objsvar.immage}" alt="team-work">
+        </div>
+        <h2 class="mt-3 grey-color">${objsvar.name}</h2>
+        <span class="mt-3 pb-3 d-block grey-color-span">${objsvar.role}</span>
+    </div>`
+};
